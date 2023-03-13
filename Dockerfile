937c687bc9850137711de79f26e3a26b696e3249
Dmitry Mikhin <dmikhin@wallarm.com>
2023-03-13 12:49:05 +0000

    prepared changes for Alpine with musl libc

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,6 @@
-FROM centos:centos7
+FROM alpine:3.14
 
 ADD scripts /build
 RUN /build/build.sh
-
-# Environment Configuration
-ENV CC=/opt/rh/devtoolset-12/root/usr/bin/gcc \
-    CPP=/opt/rh/devtoolset-12/root/usr/bin/cpp \
-    CXX=/opt/rh/devtoolset-12/root/usr/bin/g++ \
-    PATH=/opt/rh/devtoolset-12/root/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-    LD_LIBRARY_PATH=/opt/rh/devtoolset-12/root/usr/lib64:/opt/rh/devtoolset-12/root/usr/lib:/opt/rh/devtoolset-12/root/usr/lib64/dyninst:/opt/rh/devtoolset-12/root/usr/lib/dyninst \
-    MANPATH=/opt/rh/devtoolset-12/root/usr/share/man \
-    PKG_CONFIG_PATH=/opt/rh/devtoolset-12/root/usr/lib64/pkgconfig \
-    INFOPATH=/opt/rh/devtoolset-12/root/usr/share/info \
-    X_SCLS=devtoolset-12 \
-    PCP_DIR=/opt/rh/devtoolset-12/root
 
 ENTRYPOINT ["/build/entrypoint.sh"]
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,40 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
 set -ex
 
-echo '
-[devtoolset-12]
-name=Devtoolset 12
-baseurl=https://buildlogs.centos.org/c7-devtoolset-12.x86_64/
-enabled=1
-gpgcheck=0
-repo_gpgcheck=0
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7' | tee -a /etc/yum.repos.d/devtoolset-12.repo
-
-yum install -y centos-release-scl epel-release #additional repos
-yum -y install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm
-
-yum install -y devtoolset-12 sudo git \
+apk add --no-cache shadow coreutils bash sudo openssh-client \
+ autoconf automake libtool musl-dev make linux-headers \
+ gcc cmake git \
  tcl `#sqlite` \
- CUnit-devel libuuid-devel `#libwacl` \
- re2c `#libdetection` \
- texinfo `#libconfig` \
- valgrind-devel `#gperftools` \
- openssl-devel `#libproton` \
- openssl-static `#libproton proper linking into module`
-yum group install -y "Development Tools"
+ cunit-dev util-linux-dev `#libwacl` \
+ re2c bison bsd-compat-headers `#libdetection` \
+ texinfo g++ flex `#libconfig` \
+ zlib-dev `#libxml2` \
+ valgrind-dev `#gperftools` \
+ openssl-dev `#libproton` \
+ openssl-libs-static `#libproton proper linking into module` \
+ pcre-dev curl grep tar `#nginx`
 
-CMAKE_VERSION=3.25.1
-curl -L https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-Linux-x86_64.sh > cmake-${CMAKE_VERSION}-Linux-x86_64.sh && \
-sh cmake-${CMAKE_VERSION}-Linux-x86_64.sh --prefix=/usr/local/ --exclude-subdir --skip-license && \
-rm cmake-${CMAKE_VERSION}-Linux-x86_64.sh
-
-#groupadd -g ${GROUP_ID} cicd
-#useradd -m -l -u ${USER_ID} -G wheel -g cicd cicd
 echo '%wheel ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/wheel_group
 echo 'Defaults:%wheel !requiretty' >> /etc/sudoers.d/wheel_group
 
-mv -v /usr/bin/install /usr/bin/install-real
+rm /usr/bin/install
+ln -f -s /usr/bin/coreutils /tmp/install
 cp -v /build/install /usr/bin/install
-
-yum -y clean all && rm -rf /var/cache
